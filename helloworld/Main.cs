fe91826ec1bf69ed5fbd8be1a1d36234dc226d46
Robert Lillack <rob@burningsoda.com>
2012-07-13 00:26:52 +0200

    libbps: Add first event handling bits.

--- helloworld/Main.cs
+++ helloworld/Main.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 using BlackBerry;
 using BlackBerry.Screen;
 
@@ -8,9 +9,8 @@ namespace helloworld
 	{
 		public static void Main (string[] args)
 		{
-			PlatformServices.Initialize ();
-
-			using (var ctx = new Context())
+			using (var app = new PlatformServices ())
+			using (var ctx = new Context ())
 			using (var win = new Window (ctx, WindowType.SCREEN_APPLICATION_WINDOW)) {
 				win.AddBuffers (10);
 				var r = new Random();
@@ -20,17 +20,22 @@ namespace helloworld
 					System.Threading.Thread.Sleep (200);
 				}
 
-				PlatformServices.Alert("OMFG", "I'm running MONO!!!!1");
+				using (var dlg = Dialog.Show ("OMFG–“UNICODE”", "I'm running MØNØ!!!!1")) {
+					for (int i = 50; i > 0; i--) {
+						dlg.Message = String.Format("Closing in {0:0.00} seconds.", i/10.0);
+						Thread.Sleep (100);
+					}
+				}
 
 				using (var c = new Camera (Camera.Unit.Front, Camera.Mode.RW)) {
-					PlatformServices.Alert("OMFG", "I got camera!1!1");
+					Dialog.Show ("OMFG", "I got camera!1!1");
 					c.TakePhoto ();
 				}
 
+				app.NextEvent (5000);
+
 				//System.Threading.Thread.Sleep (5000);
 			}
-
-			PlatformServices.Shutdown ();
 		}
 	}
 }
--- libblackberry/PlatformServices.cs
+++ libblackberry/PlatformServices.cs
@@ -4,37 +4,200 @@ using System.Runtime.InteropServices;
 
 namespace BlackBerry
 {
-	public class PlatformServices
+	public class Dialog : IDisposable
 	{
-		[DllImport ("bps", EntryPoint = "bps_initialize")]
-		public static extern int Initialize ();
-
-		[DllImport ("bps", EntryPoint = "bps_shutdown")]
-		public static extern void Shutdown ();
+		[DllImport ("bps")]
+		static extern int dialog_set_title_text (IntPtr dialog, String title_text);
 
 		[DllImport ("bps")]
-		private static extern int dialog_set_title_text(IntPtr dialog, String title_text);
+		static extern int dialog_create_alert (out IntPtr dialog);
 
 		[DllImport ("bps")]
-		private static extern int dialog_create_alert(ref IntPtr dialog);
+		static extern int dialog_destroy (IntPtr handle);
 
 		[DllImport ("bps")]
-		private static extern int dialog_set_alert_message_text(IntPtr dialog, string text);
+		static extern int dialog_set_alert_message_text (IntPtr dialog, string text);
 
 		[DllImport ("bps")]
-		private static extern int dialog_show(IntPtr dialog);
+		static extern int dialog_show (IntPtr dialog);
 
 		[DllImport ("bps")]
-		private static extern int dialog_request_events(int flags);
+		static extern int dialog_update (IntPtr dialog);
 
-		public static void Alert(string title, string message)
+		static IDictionary<IntPtr, Dialog> dialogs = new Dictionary<IntPtr, Dialog> ();
+
+		internal Dialog FindDialog (IntPtr handle) {
+			return dialogs [handle];
+		}
+
+		public bool Visible { get; set; }
+
+		IntPtr handle;
+		public Dialog ()
 		{
-			IntPtr dialog;
+			PlatformServices.RequestDialogEvents ();
+			dialog_create_alert (out handle);
+			dialogs.Add (handle, this);
+		}
+
+		public Dialog (string title, string message) : this ()
+		{
+			Title = title;
+			Message = message;
+		}
+
+		public static Dialog Show (string title, string message)
+		{
+			var a = new Dialog (title, message);
+			a.Show ();
+			return a;
+		}
+
+		public string Title {
+			get {
+				// TODO: implement dialog_get_title_text ()
+				return "";
+			}
+			set {
+				dialog_set_title_text (handle, value);
+				if (Visible) {
+					dialog_update (handle);
+				}
+			}
+		}
+
+		public string Message {
+			get {
+				// TODO: implement dialog_get_alert_message_text ()
+				return "";
+			}
+			set {
+				dialog_set_alert_message_text (handle, value);
+				if (Visible) {
+					dialog_update (handle);
+				}
+			}
+		}
+
+		public void Show ()
+		{
+			dialog_show (handle);
+			Visible = true;
+		}
+
+		public void Dispose ()
+		{
+			dialog_destroy (handle);
+			Visible = false;
+			dialogs.Remove (handle);
+		}
+	}
+
+	public class Event
+	{
+		[DllImport ("bps")]
+		static extern uint bps_event_get_code (IntPtr handle);
+
+		[DllImport ("bps")]
+		static extern IntPtr bps_event_get_payload (IntPtr handle);
+
+		protected IntPtr handle;
+
+		internal Event (IntPtr h)
+		{
+			handle = h;
+		}
+
+		public uint Code {
+			get {
+				return bps_event_get_code (handle);
+			}
+		}
+	}
+
+	public class DialogEvent : Event
+	{
+		[DllImport ("bps")]
+		static extern IntPtr dialog_event_get_dialog_instance (IntPtr handle);
+
+		internal DialogEvent (IntPtr h) : base (h) {}
+
+		public Dialog Dialog {
+			get {
+				return Dialog.FindDialog (dialog_event_get_dialog_instance (handle));
+			}
+		}
+	}
+
+	public class PlatformServices : IDisposable
+	{
+		[DllImport ("bps")]
+		static extern int bps_initialize ();
+
+		[DllImport ("bps")]
+		static extern void bps_shutdown ();
+
+		[DllImport ("bps")]
+		static extern void bps_get_event (out IntPtr handle, int timeout_ms);
+
+		[DllImport ("bps")]
+		static extern int dialog_request_events (int flags);
+
+		[DllImport ("bps")]
+		static extern int dialog_get_domain ();
+
+		[DllImport ("bps")]
+		static extern int bps_event_get_domain (IntPtr handle);
+
+		static bool initialized = false;
+		static bool dialogEventsRequested = false;
+
+		public PlatformServices ()
+		{
+			Initialize ();
+		}
+
+		public static void Initialize ()
+		{
+			if (initialized) {
+				return;
+			}
+
+			bps_initialize ();
+			initialized = true;
+		}
+
+		public static void RequestDialogEvents ()
+		{
+			if (dialogEventsRequested) {
+				return;
+			}
+
+			Initialize ();
 			dialog_request_events (0);
-			dialog_create_alert(ref dialog);
-			dialog_set_title_text(dialog, title);
-			dialog_set_alert_message_text(dialog, message);
-			dialog_show(dialog);
+			dialogEventsRequested = true;
+		}
+
+		public Event NextEvent ()
+		{
+			return NextEvent (-1);
+		}
+
+		public Event NextEvent (int timeoutMillis)
+		{
+			IntPtr handle;
+			bps_get_event (out handle, timeoutMillis);
+
+			if (dialogEventsRequested && bps_event_get_domain (handle) == dialog_get_domain ()) {
+				return new DialogEvent (handle);
+			}
+
+			return new Event (handle);
+		}
+
+		public void Dispose ()
+		{
+			bps_shutdown ();
 		}
 	}
 }
