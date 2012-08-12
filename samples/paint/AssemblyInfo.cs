2d89822067886e820d129d5c31786b2d9ba6012a
Robert Lillack <rob@burningsoda.com>
2012-08-12 21:50:30 +0200

    Add 'paint' sample.

---
+++ samples/paint/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+using BlackBerry.ApplicationDescriptor;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// <name>: Title of the Application on the home screen
+[assembly: AssemblyTitle("MonoBerry Paint")]
+// <description>: …
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Robert Lillack <rob@burningsoda.com>, burningsoda.com")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// <id>: Unique Identifier of the Application
+[assembly: ApplicationIdentifier ("com.burningsoda.monoberry.samples.paint")]
+// <action> …
+[assembly: RequestedPermissions (Action.AccessInternet, Action.UseCamera)]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+// <versionNumber> + <buildId>
+[assembly: AssemblyVersion("1.0.*")]
---
+++ samples/paint/Main.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Threading;
+using BlackBerry;
+using BlackBerry.Screen;
+
+namespace MonoBerry.Samples.Paint
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			using (var nav = new Navigator ())
+			using (var ctx = new Context ())
+			using (var win = new Window (ctx)) {
+				win.Usage = Usage.SCREEN_USAGE_NATIVE;
+				win.AddBuffers (2);
+				var bufs = win.Buffers;
+				var pic = bufs[0];
+				var brush = bufs[1];
+
+				pic.Fill (0xffff0000);
+				brush.Fill (0xff000000);
+				win.Render (pic);
+
+				//nav.OnSwipeDown = () => Dialog.Alert ("#MonoBerry", "Another Event Loop", new Button ("Ack"));
+				ctx.OnFingerTouch = (x,y) => {
+					pic.Blit (brush, 0, 0, 10, 10, Math.Max (x - 5, 0), Math.Max (y - 5, 0));
+					win.Render (pic);
+				};
+				ctx.OnFingerMove = ctx.OnFingerTouch;
+				ctx.OnFingerRelease = ctx.OnFingerTouch;
+
+				PlatformServices.Run ();
+			}
+		}
+	}
+}
